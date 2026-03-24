27c5813d7581b94f41a1a45b6bc8a298bd342b31
El profe <al.sepulvedam@profesor.duoc.cl>
2026-03-24 12:05:55 -0300

    get libros agregado

---
+++ src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -0,0 +1,26 @@
+package com.example.bibliotecaduoc.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bibliotecaduoc.model.Libro;
+import com.example.bibliotecaduoc.service.LibroService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+@RequestMapping("/api/v1/libros")
+public class LibroController {
+    @Autowired
+    private LibroService libroService;
+
+    @GetMapping
+    public List<Libro> listaLibros() {
+       return libroService.readAll();
+    }
+    
+}
